08dbc9e5dd82970a611e106952e50749bd9e0f80
Gio Piantoni <github@gpiantoni.com>
2014-02-14 19:10:59 -0500

    make figures and create table for wiki, very neat

--- eloc/elec_info.py
+++ eloc/elec_info.py
@@ -3,6 +3,7 @@ from subprocess import call
 from tempfile import mkdtemp
 from visvis import gca, record
 
+from phypno.attr.chan import find_chan_in_region, assign_region_to_channels
 from phypno.viz.plot_3d import plot_surf, plot_chan
 
 from .snap_grid_to_pial import is_grid
@@ -28,6 +29,8 @@ def _plot_grid_and_depth_elec(hemi_chan):
 def plot_rotating_brains(chan, anat, gif_file):
     """Plot the two hemispheres including the electrodes.
 
+    Parameters
+    ----------
     chan : instance of phypno.attr.chan.Channels
         channels to plot
     anat : instance of phypno.attr.anat.Freesurfer
@@ -65,6 +68,41 @@ def plot_rotating_brains(chan, anat, gif_file):
 
         rec.Export(join(img_dir, 'image.png'))
 
-        hemi_gif = splitext(gif_file)[0] + '-' + hemi + splitext(gif_file)[1]
+        hemi_gif = splitext(gif_file)[0] + '_' + hemi + splitext(gif_file)[1]
         call('convert ' + join(img_dir, 'image*.png') + ' ' + hemi_gif,
              shell=True)
+
+
+def make_table_of_regions(chan, anat, wiki_table):
+    """Write location of the channels as wiki page.
+
+    Parameters
+    ----------
+    chan : instance of phypno.attr.chan.Channels
+        channels to plot
+    anat : instance of phypno.attr.anat.Freesurfer
+        anatomy to plot
+    wiki_table : str
+        path to write wiki table.
+
+    """
+    assign_region_to_channels(chan, anat)
+    neuroport = chan(lambda x: x.label == 'neuroport')
+    depth_chan = chan(lambda x: not is_grid(x))
+
+    with open(wiki_table, 'w') as f:
+
+        if len(neuroport.chan) == 1:
+            f.write('Neuroport in ' + neuroport.chan[0].attr['region']
+                    + '\n')
+
+        f.write('\n^ Regions ^ Electrodes ^\n')
+        for region in sorted(set(depth_chan.return_attr('region'))):
+            chan_in_region = find_chan_in_region(depth_chan, anat, region)
+            f.write('| {} | {} |\n'.format(region, ', '.join(chan_in_region)))
+
+        f.write('\n^ Electrode ^ Distance ^ Regions ^ \n')
+        for one_chan in depth_chan.chan:
+            f.write('| {0} | {1} | {2} |\n'.format(one_chan.label,
+                                                   one_chan.attr['approx'],
+                                                   one_chan.attr['region']))
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ path.append('/home/gio/projects/eloc/scripts')
 
 from phypno.attr import Channels, Freesurfer
 
-
 from rcmg.interfaces import make_struct
 from eloc.snap_grid_to_pial import adjust_grid_strip_chan
+from eloc.elec_info import plot_rotating_brains, make_table_of_regions
 
 lg = getLogger('eloc')
 lg.setLevel(INFO)
@@ -28,6 +28,13 @@ for subj in sorted(listdir(recdir)):
 
     adjust_grid_strip_chan(chan, anat)
     chan.export(adj_elec_file)
+
+    gif_file = join(dir_names['doc_wiki'], 'elec_pos.gif')
+    plot_rotating_brains(chan, anat, gif_file)
+
+    wiki_file = join(dir_names['doc_wiki'], 'elec_pos_table.txt')
+    make_table_of_regions(chan, anat, wiki_file)
+
 
 
 """
